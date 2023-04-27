904a8b3ef69f8e15f623ffe60db0f66b41405bd8
Thomas Lescieux <thomas.lescieux@school-by-hiit.fr>
2023-04-27 09:50:49 +0200

    feat: first commit

---
+++ src/main/java/fr/lescieux/devopscrashtest/DevopsCrashTestApplication.java
@@ -0,0 +1,13 @@
+package fr.lescieux.devopscrashtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsCrashTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsCrashTestApplication.class, args);
+	}
+
+}
